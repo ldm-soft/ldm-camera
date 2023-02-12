adfe40da7aaea5989c35d681e0f00717bfbdd0cb
linhdinhdev9x <linhdinh2805@gmail.com>
2023-02-12 23:54:10 +0700

    UPD

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import ConfigContainer from './component/ConfigContainer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
+
+function BlogPost() {
+  let { slug } = useParams();
+  return <div>Now showing post {slug}</div>;
+}
+
 ReactDOM.render(
-  // <App />,
-  <ConfigContainer />,
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route exact path="/config">
+        <ConfigContainer />
+      </Route>
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
